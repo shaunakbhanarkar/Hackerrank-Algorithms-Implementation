455aa62455a5e2be30f0ca3be8db1b21d027592a
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-17 20:12:15 +0530

    Create beautifulday.cpp

---
+++ beautifulday.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int reverse(int x)
+{
+    int ans=0;
+    while (x>0)
+    {
+        ans = 10*ans + x%10;
+        x=x/10;   
+    }
+    return ans;
+}
+
+// Complete the beautifulDays function below.
+int beautifulDays(int i, int j, int k) {
+        int count=0;
+        for (int x=i; x<=j; x++)
+        {
+            if (abs(x-reverse(x))%k == 0)
+                count++;
+        }
+        return count;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string ijk_temp;
+    getline(cin, ijk_temp);
+
+    vector<string> ijk = split_string(ijk_temp);
+
+    int i = stoi(ijk[0]);
+
+    int j = stoi(ijk[1]);
+
+    int k = stoi(ijk[2]);
+
+    int result = beautifulDays(i, j, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
